55f9a1ac27bd680ea9116b664b9e620fcc5d8c2e
John Crocker <johnniecrocker@yahoo.com>
2014-10-02 14:55:52 -0400

    Header selected in js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
 console.log('The Iron Yard Rocks');
+
+var header1 = document.getElementById('Header1');
+
+
+
+$( document.body ).click(function() {
+  $( "div" ).each(function( i ) {
+    if ( this.height !== "350px" ) {
+      this.height = "350px";
+    } else {
+      this.height = "";
+    }
+  });
+});
