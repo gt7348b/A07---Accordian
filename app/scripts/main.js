cd2f6f9eeeaa186d60c0686cd1912aad0a841573
John Crocker <johnniecrocker@yahoo.com>
2014-10-02 15:37:45 -0400

    working on changing height with click

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,13 @@
 console.log('The Iron Yard Rocks');
 
-var header1 = document.getElementById('Header1');
-
 $( "#Header1" ).click(function() {
-  alert( "Handler for .click() called." );
+  if ($(this).css("height") === "50px") {
+        $(this).css ("height" 350px);
+          } else {
+            $(this).css("height" "50px")
+    };)
+    };
+
 });
 
 $( "#Header2" ).click(function() {
