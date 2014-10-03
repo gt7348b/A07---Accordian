a4fe3c73fd9421191bd2ea045b679a02cffbdea1
John Crocker <johnniecrocker@yahoo.com>
2014-10-03 12:09:27 -0400

    Simplified css with only click class

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,19 +2,19 @@ console.log('The Iron Yard Rocks');
 
 
 $( "#Header1" ).click(function() {
-        $('#Header1').toggleClass("click1");
-        $("#Header2").removeClass("click2");
-        $("#Header3").removeClass("click3");
+        $('#Header1').toggleClass("click");
+        $("#Header2").removeClass("click");
+        $("#Header3").removeClass("click");
 });
 
 $( "#Header2" ).click(function() {
-        $("#Header2").toggleClass("click2");
-        $("#Header1").removeClass("click1");
-        $("#Header3").removeClass("click3");
+        $("#Header2").toggleClass("click");
+        $("#Header1").removeClass("click");
+        $("#Header3").removeClass("click");
 });
 
 $( "#Header3" ).click(function() {
-        $("#Header3").toggleClass("click3");
-        $("#Header1").removeClass("click1");
-        $("#Header2").removeClass("click2");
+        $("#Header3").toggleClass("click");
+        $("#Header1").removeClass("click");
+        $("#Header2").removeClass("click");
 });
