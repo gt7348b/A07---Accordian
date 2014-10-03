328c92c4a37df403658484706c3da1dca509954b
John Crocker <johnniecrocker@yahoo.com>
2014-10-03 10:02:38 -0400

    Working need to format

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,5 @@
 console.log('The Iron Yard Rocks');
 
-var header1 = document.getElementById('Header1')
-
 
 $( "#Header1" ).click(function() {
         $('#Header1').toggleClass("click1");
@@ -10,14 +8,13 @@ $( "#Header1" ).click(function() {
 });
 
 $( "#Header2" ).click(function() {
-  $("#Header2").toggleClass("click2");
-  $("#Header1").removeClass("click1");
-  $("#Header3").removeClass("click3");
+        $("#Header2").toggleClass("click2");
+        $("#Header1").removeClass("click1");
+        $("#Header3").removeClass("click3");
 });
 
 $( "#Header3" ).click(function() {
-  $("#Header3").toggleClass("click3");
-
-  $("#Header1").removeClass("click1");
-  $("#Header2").removeClass("click2");
+        $("#Header3").toggleClass("click3");
+        $("#Header1").removeClass("click1");
+        $("#Header2").removeClass("click2");
 });
