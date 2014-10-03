6f8aacf2884f69aa765c5f22819c120ff11a668d
John Crocker <johnniecrocker@yahoo.com>
2014-10-02 21:11:15 -0400

    fixed hover for background color

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,23 +9,20 @@ $( '#Body3' ).hide();
 $( "#Header1" ).click(function() {
         $('#Header1').toggleClass("click1");
         $( '#Body1' ).toggle();
-
-
-  /*if ($(this).css("height") === "50px") {
-        $(this).css ("height" 350px);
-          } else {
-            $(this).css("height" "50px")
-    };)
-  };*/
-
+        $( '#Body2' ).hide();
+        $( '#Body3' ).hide();
 });
 
 $( "#Header2" ).click(function() {
   $("#Header2").toggleClass("click2");
-  $( '#Body2' ).toggle();
+  $( '#Body2' ).toggle( 5000 );
+  $( '#Body3' ).hide();
+  $( '#Body1' ).hide();
 });
 
 $( "#Header3" ).click(function() {
   $("#Header3").toggleClass("click3");
   $( '#Body3' ).toggle();
+  $( '#Body1' ).hide();
+  $( '#Body2' ).hide();
 });
