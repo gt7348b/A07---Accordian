7aa00663ee724c53f88f7ba130647018c079d07f
John Crocker <johnniecrocker@yahoo.com>
2014-10-03 09:44:50 -0400

    Kinda working on scroll with overflow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,13 +2,10 @@ console.log('The Iron Yard Rocks');
 
 var header1 = document.getElementById('Header1')
 
-$( '#Body1' ).hide();
-$( '#Body2' ).hide();
-$( '#Body3' ).hide();
 
 $( "#Header1" ).click(function() {
         $('#Header1').toggleClass("click1");
-        $( '#Body1' ).toggle( 5000 );
+        $( '#Body1' ).toggle();
         $( '#Body2' ).hide();
         $( '#Body3' ).hide();
         $("#Header2").removeClass("click2");
@@ -17,7 +14,7 @@ $( "#Header1" ).click(function() {
 
 $( "#Header2" ).click(function() {
   $("#Header2").toggleClass("click2");
-  $( '#Body2' ).toggle( 5000 );
+  $( '#Body2' ).toggle();
   $( '#Body3' ).hide();
   $( '#Body1' ).hide();
   $("#Header1").removeClass("click1");
@@ -26,9 +23,9 @@ $( "#Header2" ).click(function() {
 
 $( "#Header3" ).click(function() {
   $("#Header3").toggleClass("click3");
-  $( '#Body3' ).toggle( 5000 );
-  $( '#Body1' ).hide();
+  $( '#Body3' ).toggle();
   $( '#Body2' ).hide();
+  $( '#Body1' ).hide();
   $("#Header1").removeClass("click1");
   $("#Header2").removeClass("click2");
 });
