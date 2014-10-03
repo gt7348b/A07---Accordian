3fbe74f76b1e95d5f1195512de12bbf160250450
John Crocker <johnniecrocker@yahoo.com>
2014-10-02 20:28:48 -0400

    Got toggle working on body text

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,14 @@ console.log('The Iron Yard Rocks');
 
 var header1 = document.getElementById('Header1')
 
+$( '#Body1' ).hide();
+$( '#Body2' ).hide();
+$( '#Body3' ).hide();
+
 $( "#Header1" ).click(function() {
-        $("#Header1").toggleClass("click1");
+        $('#Header1').toggleClass("click1");
+        $( '#Body1' ).toggle();
+
 
   /*if ($(this).css("height") === "50px") {
         $(this).css ("height" 350px);
@@ -16,8 +22,10 @@ $( "#Header1" ).click(function() {
 
 $( "#Header2" ).click(function() {
   $("#Header2").toggleClass("click2");
+  $( '#Body2' ).toggle();
 });
 
 $( "#Header3" ).click(function() {
   $("#Header3").toggleClass("click3");
+  $( '#Body3' ).toggle();
 });
